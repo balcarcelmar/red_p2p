732bf194fc7ca7f46c70e54404134205b97b8a6c
Darch02 <juanajaramillom@hotmail.com>
2024-09-18 20:17:06 -0500

    se cambiarion todos los archivos para que usaran las ips y las rutas en el archivo de configuración, y se probaron las funciones de upload y download

--- redP2P/files/peer4/peer4.py
+++ redP2P/files/peer4/peer4.py
@@ -21,7 +21,7 @@ def register_with_directory():
         "peer_ip": f"http://{config['ip']}:{config['port']}",
         "files": list_files()
     }
-    response = requests.post(f"http://localhost:3000/login", json=peer_data)
+    response = requests.post(config['peer_titular'], json=peer_data)
     print("Registro del peer:", response.json())
 
 @app.route('/get_files', methods=['GET'])
@@ -40,4 +40,3 @@ def upload_file():
 
 if __name__ == '__main__':
     register_with_directory()
-    app.run(host=config['ip'], port=config['port'])
